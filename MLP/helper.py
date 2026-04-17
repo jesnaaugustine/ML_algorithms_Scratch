9701b10acb4cc89ed22e2e2d7c15d76bd05d9b5c
jesnaaugustine <jesnatrisa@gmail.com>
2026-04-17 16:15:13 +0530

    MLP last

--- MLP/helper.py
+++ MLP/helper.py
@@ -5,6 +5,7 @@ import torch
 import numpy as np
 import random
 import time
+import matplotlib.pyplot as plt
 
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
@@ -27,9 +28,10 @@ class MyDataset(Dataset):
 
 def my_dataloader(train_set,test_set,train_target,test_target,batch_size,valid_fraction=0.1):
     valid_fraction = int(train_set.shape[0]*valid_fraction)
-    train_dataset=Dataset(train_set[:valid_fraction],train_target[:valid_fraction])
-    valid_dataset=Dataset(train_set[valid_fraction:],train_target[:valid_fraction:])
-    test_dataset=Dataset(test_set,test_target)
+    print(valid_fraction)
+    train_dataset=MyDataset(train_set[:valid_fraction],train_target[:valid_fraction])
+    valid_dataset=MyDataset(train_set[valid_fraction:],train_target[valid_fraction:])
+    test_dataset=MyDataset(test_set,test_target)
 
     train_loader =DataLoader(train_dataset,batch_size=batch_size,shuffle=True)
     valid_loader =DataLoader(valid_dataset,batch_size=batch_size,shuffle=True)
@@ -37,13 +39,126 @@ def my_dataloader(train_set,test_set,train_target,test_target,batch_size,valid_f
 
     return train_loader,valid_loader,test_loader
 
+def comp_accuracy(model,dataloader,device):
+    with torch.no_grad():
+        correct_pred =0
+        num_examples =0
+        for i, (features,target) in enumerate(dataloader):
+            features =features.to(device)
+            target = target.float().to(device)
 
+            logit = model.forward(features)
+            predicted =torch.argmax(logit,dim=1)
+            num_examples+=target.size(0)
+            correct_pred+=(predicted==target).sum()
+        return (correct_pred.float()/num_examples)*100
 
 def train_model(model,num_epoch,train_loader,valid_loader,test_loader,optimizer,device):
     start_time = time.time()
     minibatch_loss_list, train_acc_list, valid_acc_list = [], [], []
     for epoch in range(num_epoch):
         model.train()
+        for batch_idx,(features,target) in enumerate(train_loader):
+            features = features.to(device)
+            target = target.to(device)
 
+            # ## FORWARD AND BACK PROP
+            logits = model.forward(features)
+            loss = torch.nn.functional.cross_entropy(logits, target)
+            optimizer.zero_grad()
+
+            loss.backward()
+
+            # ## UPDATE MODEL PARAMETERS
+            optimizer.step()
+
+            # ## LOGGING
+            minibatch_loss_list.append(loss.item())
+            if not batch_idx % 50:
+                print(f'Epoch: {epoch+1:03d}/{num_epoch:03d} '
+                      f'| Batch {batch_idx:04d}/{len(train_loader):04d} '
+                      f'| Loss: {loss:.4f}')
+            
+        model.eval()
+        
+        with torch.no_grad():  # save memory during inference
+            train_acc = comp_accuracy(model, train_loader, device=device)
+            valid_acc = comp_accuracy(model, valid_loader, device=device)
+            print(f'Epoch: {epoch+1:03d}/{num_epoch:03d} '
+                f'| Train: {train_acc :.2f}% '
+                f'| Validation: {valid_acc :.2f}%')
+            train_acc_list.append(train_acc.item())
+            valid_acc_list.append(valid_acc.item())
+
+        elapsed = (time.time() - start_time)/60
+        print(f'Time elapsed: {elapsed:.2f} min')
+    elapsed = (time.time() - start_time)/60
+    print(f'Total Training Time: {elapsed:.2f} min')
+
+    test_acc = comp_accuracy(model, test_loader, device=device)
+    print(f'Test accuracy {test_acc :.2f}%')
+
+    return minibatch_loss_list, train_acc_list, valid_acc_list
+
+def plot_accuracy(train_acc_list, valid_acc_list, results_dir):
+
+    num_epochs = len(train_acc_list)
+
+    plt.plot(np.arange(1, num_epochs+1),
+             train_acc_list, label='Training')
+    plt.plot(np.arange(1, num_epochs+1),
+             valid_acc_list, label='Validation')
+
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.legend()
+
+    plt.tight_layout()
+    image_path = os.path.join(results_dir, 'plot_acc_training_validation.pdf')
+    plt.savefig(image_path)
+    plt.clf()
+
+def plot_training_loss(minibatch_loss_list, num_epochs, iter_per_epoch,
+                       results_dir, averaging_iterations=100):
+
+    plt.figure()
+    ax1 = plt.subplot(1, 1, 1)
+    ax1.plot(range(len(minibatch_loss_list)),
+             (minibatch_loss_list), label='Minibatch Loss')
+
+    if len(minibatch_loss_list) > 1000:
+        ax1.set_ylim([
+            0, np.max(minibatch_loss_list[1000:])*1.5
+            ])
+    ax1.set_xlabel('Iterations')
+    ax1.set_ylabel('Loss')
+
+    ax1.plot(np.convolve(minibatch_loss_list,
+                         np.ones(averaging_iterations,)/averaging_iterations,
+                         mode='valid'),
+             label='Running Average')
+    ax1.legend()
+
+    ###################
+    # Set scond x-axis
+    ax2 = ax1.twiny()
+    newlabel = list(range(num_epochs+1))
+
+    newpos = [e*iter_per_epoch for e in newlabel]
+
+    ax2.set_xticks(newpos[::10])
+    ax2.set_xticklabels(newlabel[::10])
+
+    ax2.xaxis.set_ticks_position('bottom')
+    ax2.xaxis.set_label_position('bottom')
+    ax2.spines['bottom'].set_position(('outward', 45))
+    ax2.set_xlabel('Epochs')
+    ax2.set_xlim(ax1.get_xlim())
+    ###################
+
+    plt.tight_layout()
+    image_path = os.path.join(results_dir, 'plot_training_loss.pdf')
+    plt.savefig(image_path)
+    plt.clf()
 
 
--- MLP/train_model.py
+++ MLP/train_model.py
@@ -5,7 +5,7 @@ import os
 import time
 import yaml
 
-from helper import set_all_seed,my_dataloader
+from helper import set_all_seed,my_dataloader,train_model,plot_accuracy,plot_training_loss,comp_accuracy
 import pandas as pd
 
 parser = argparse.ArgumentParser()
@@ -17,14 +17,14 @@ if not os.path.exists(args.output_path):
     os.makedirs(args.output_path)
 
 with open(args.setting_path) as file:
-    SETTINGS= yaml.load(file)
+    SETTINGS= yaml.load(file,Loader=yaml.FullLoader)
 
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s'
 )
 logger = logging.getLogger()
-logpath = os.path.join(args.results_path, 'training.log')
+logpath = os.path.join(args.output_path, 'training.log')
 logger.addHandler(logging.FileHandler(logpath, 'a'))
 print = logger.info
 
@@ -40,7 +40,7 @@ data = pd.read_csv(SETTINGS['data path'])
 X =data[SETTINGS['features']]
 y =data[SETTINGS['target']]
 
-y.map(SETTINGS['map'])
+y=y.map(SETTINGS['map'])
 
 X = torch.tensor(X.values,dtype =torch.float)
 y = torch.tensor(y.values,dtype=torch.float)
@@ -48,8 +48,8 @@ shuffle_idx = torch.randperm(X.size(0),dtype =torch.int)
 X,y = X[shuffle_idx],y[shuffle_idx]
 
 perc80 = int(shuffle_idx.size(0)*0.8)
-X_train,X_test = X[shuffle_idx[:perc80]],X[shuffle_idx[perc80:]]
-y_train,y_test = y[shuffle_idx[:perc80]],y[shuffle_idx[perc80:]]
+X_train,X_test = X[:perc80],X[perc80:]
+y_train,y_test = y[:perc80],y[perc80:]
 
 mu,sigma = X_train.mean(dim =0),X_train.std(dim=0)
 
@@ -58,6 +58,10 @@ X_test =(X_test-mu)/sigma
 
 train_loader,valid_loader,test_loader = my_dataloader(X_train,X_test,y_train,y_test,batch_size=SETTINGS['batch size'])
 for x_batch,y_batch in train_loader:
+    print(f'X tain shape: {x_batch.shape}')
+    print(f'Y tain shape: {y_batch.shape}')
+    break
+for x_batch,y_batch in valid_loader:
     print(f'X tain shape: {x_batch.shape}')
     print(f'Y tain shape: {y_batch.shape}')
     break
@@ -74,12 +78,42 @@ class MLP(torch.nn.Module):
 
         )
     
-    def forword(self,X):
+    def forward(self,X):
         return self.model(X)
     
 model= MLP(num_features=X_train.shape[1],num_hidden=SETTINGS['hidden layer size'],num_classes=SETTINGS['num class labels'])
 model=model.to(device)
 optimizer=torch.optim.SGD(model.parameters(),lr =SETTINGS['learning rate'])
+
+minibatch_loss_list, train_acc_list, valid_acc_list = train_model(
+    model=model,
+    num_epoch=SETTINGS['num epochs'],
+    train_loader=train_loader,
+    valid_loader=valid_loader,
+    test_loader=test_loader,
+    optimizer=optimizer,
+    device=device)
+
+test_acc = comp_accuracy(model, test_loader, device=device)
+print(f'Test accuracy {test_acc :.2f}%')
+
+plot_training_loss(minibatch_loss_list=minibatch_loss_list,
+                   num_epochs=SETTINGS['num epochs'],
+                   iter_per_epoch=len(train_loader),
+                   results_dir=args.output_path)
+plot_accuracy(train_acc_list=train_acc_list,
+              valid_acc_list=valid_acc_list,
+              results_dir=args.output_path)
+
+results_dict = {'train accuracies': train_acc_list,
+                'validation accuracies': valid_acc_list,
+                'test accuracy': test_acc.item(),
+                'settings': SETTINGS}
+
+output_path = os.path.join(args.output_path, 'results_dict.yaml')
+with open(output_path, 'w') as file:
+    yaml.dump(results_dict, file)
+
 
 
 
